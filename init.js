2af6eb6ac12b7ef87e36b877e3f012bd9d3b054e
BAAARCODE <28970454+BAAARCODE@users.noreply.github.com>
2022-01-16 23:19:28 -0500

    add text colour/highlighting functionality

--- init.js
+++ init.js
@@ -1,15 +1,25 @@
-function initPalette(parentID, colours)
+function initPalette(parentID, command, colours)
 {
     var p = document.getElementById(parentID);
 
     for (var i = 0; i < colours.length; i++)
     {
-        var node = document.createElement("DIV");
-        node.style.backgroundColor = colours[i];
+        const colour = colours[i];
+        const node = document.createElement("DIV");
+        node.style.backgroundColor = colour;
         node.classList.add("tool");
+
+        node.onclick = function()
+        {
+            format('styleWithCSS', true);
+            format(command, node.style.backgroundColor);
+            format('styleWithCSS', false);
+            console.log(node.style.backgroundColor);
+        }
+
         p.appendChild(node);
     }
 }
 
-initPalette("text-colours", textColours);
-initPalette("highlight-colours", highlightColours);
+initPalette("text-colours", "foreColor", textColours);
+initPalette("highlight-colours", "hiliteColor", highlightColours);
